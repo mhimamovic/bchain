777184b3ca5321086491cbc9acf7df76659093a5
mhimamovic <48456077+mhimamovic@users.noreply.github.com>
2021-04-06 00:13:40 +0200

    Add files via upload

---
+++ styles/main.js
@@ -0,0 +1,14 @@
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const hamburgerX = document.querySelector('.btnHamburger-x');
+
+btnHamburger.addEventListener('click', function(){
+    if(header.classList.contains('open')){
+        header.classList.remove('open');
+    }
+    else {
+        header.classList.add('open');
+    }
+});
+
+
